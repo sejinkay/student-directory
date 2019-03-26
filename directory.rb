3ed5ada3b4db6349d436fa5270da2ffb318e8412
sejinkay <sejinkay@gmail.com>
2019-03-26 19:36:50 +0000

    refactored a bit

--- directory.rb
+++ directory.rb
@@ -15,59 +15,66 @@ students = [
 ]
 =end
 
+@students = [] # an empty array acessible to all methods
+
 def interactive_menu
-	students =[]
 	loop do 
-		# 1. print the menu and ask the user what to do
+		print_menu
+		process(gets.chomp)
+	end
+end
+
+def print_menu
 		puts "1. Input the students"
 		puts "2. Show the students"
 		puts "9. Exit" # 9 because we'll be adding more items
-		# 2. read the input and save it into a variable
-		selection = gets.chomp
-		# 3. do what the user has asked
+end
+
+def show_students
+			print_header
+			print_student_list
+			print_footer
+end
+
+def process(selection)
 		case selection
 		when "1"
-			students = input_students
+			input_students
 		when "2"
-			print_header
-			print(students)
-			print_footer(students)
+			show_students
 		when "9"
-			exit # this will cause the program to terminate
+			exit
 		else
 			puts "I don't know what you meant, try again"
 		end
-	end
 end
+
 
 def print_header
 	puts "The students of Villains Academy"
 	puts "-------------"
 end
-def print(students)
-	students.each do |student|
+
+def print_student_list
+	@students.each do |student|
 		puts "#{student[:name]} (#{student[:cohort]} cohort)"
 	end
 end
-def print_footer(names)
-	puts "Overall, we have #{names.count} great students"
+def print_footer
+	puts "Overall, we have #{@students.count} great students"
 end
 
 def input_students
 	puts "Please enter the name of the students"
 	puts "To finish, just hit return twice"
-	# creat an empty array
-	students = []
 	name = gets.chomp
 	while !name.empty? do
 		# add the student hash to the array
-		students << {name: name, cohort: :november}
-		puts "Now we have #{students.count} students"
+		@students << {name: name, cohort: :november}
+		puts "Now we have #{@students.count} students"
 		# get another name from the user
 		name = gets.chomp
 	end
-	# return the array of students
-	students
 end
 
 interactive_menu
