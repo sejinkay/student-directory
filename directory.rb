4278741314b40ce0235402b119a79a68beef5eb4
sejinkay <sejinkay@gmail.com>
2019-03-31 23:48:17 +0100

    added try_load_students methods

--- directory.rb
+++ directory.rb
@@ -20,7 +20,7 @@ students = [
 def interactive_menu
 	loop do 
 		print_menu
-		process(gets.chomp)
+		process(STDIN.gets.chomp)
 	end
 end
 
@@ -73,13 +73,13 @@ end
 def input_students
 	puts "Please enter the name of the students"
 	puts "To finish, just hit return twice"
-	name = gets.chomp
+	name = STDIN.gets.chomp
 	while !name.empty? do
 		# add the student hash to the array
 		@students << {name: name, cohort: :november}
 		puts "Now we have #{@students.count} students"
 		# get another name from the user
-		name = gets.chomp
+		name = STDIN.gets.chomp
 	end
 end
 
@@ -95,7 +95,7 @@ def save_students
 	file.close
 end
 
-def load_students
+def load_students(fliename= "students.csv")
 	file = File.open("students.csv", "r")
 	file.readlines.each do |line|
 		name, cohort = line.chomp.split(',')
@@ -104,4 +104,17 @@ def load_students
 	file.close
 end
 
+def try_load_students
+	filename = ARGV.first # first argument from the command line
+	return if filename.nil? # get out of the method if it isn't given
+	if File.exits?(filename) # if it exists
+		load_students(filename)
+		puts "Loaded #{@students.count} from #{filename}"
+	else # if it doesn't exist
+		puts "Sorry, #{filenmae} doesn't exist."
+		exit # quit the program
+	end
+end
+
+try_load_students
 interactive_menu
